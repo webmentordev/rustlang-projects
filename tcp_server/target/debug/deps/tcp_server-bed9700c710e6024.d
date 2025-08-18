07f76195b546ad29c4489b4a498c60962767d32c
webmentordev <ahmertahir99@gmail.com>
2025-08-18 22:02:45 +0500

    TCP Server Target fix

--- tcp_server/target/debug/deps/tcp_server-bed9700c710e6024.d
+++
@@ -1,5 +0,0 @@
-/home/ahmer/Downloads/rust/rust-projects-practice/tcp_server/target/debug/deps/tcp_server-bed9700c710e6024.d: src/main.rs
-
-/home/ahmer/Downloads/rust/rust-projects-practice/tcp_server/target/debug/deps/tcp_server-bed9700c710e6024: src/main.rs
-
-src/main.rs:
--- tcp_server/target/debug/tcp_server.d
+++
@@ -1,1 +0,0 @@
-/home/ahmer/Downloads/rust/rust-projects-practice/tcp_server/target/debug/tcp_server: /home/ahmer/Downloads/rust/rust-projects-practice/tcp_server/src/main.rs
