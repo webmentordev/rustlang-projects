732b5dcc9846cf90d3523f3b0b6eadc8c0378c27
webmentordev <ahmertahir99@gmail.com>
2026-08-08 14:50:42 +0500

    TryInto terget deleted

--- effective_rust/program/target/debug/deps/program-6db6b0807f0e3d8e.d
+++
@@ -1,5 +0,0 @@
-/home/ahmer/Downloads/rust/rustlang-projects/effective_rust/program/target/debug/deps/program-6db6b0807f0e3d8e.d: src/main.rs
-
-/home/ahmer/Downloads/rust/rustlang-projects/effective_rust/program/target/debug/deps/program-6db6b0807f0e3d8e: src/main.rs
-
-src/main.rs:
--- effective_rust/program/target/debug/program.d
+++
@@ -1,1 +0,0 @@
-/home/ahmer/Downloads/rust/rustlang-projects/effective_rust/program/target/debug/program: /home/ahmer/Downloads/rust/rustlang-projects/effective_rust/program/src/main.rs
